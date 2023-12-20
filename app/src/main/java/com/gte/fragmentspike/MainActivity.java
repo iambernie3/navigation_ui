69a4f1967eba1102eec2e17e7b9440f3e1d22ad2
bernie <bernie.tuwain@got-the-experts.com>
2023-12-20 22:24:46 +0800

    set up toolbar

--- app/src/main/java/com/gte/fragmentspike/MainActivity.java
+++ app/src/main/java/com/gte/fragmentspike/MainActivity.java
@@ -1,14 +1,40 @@
 package com.gte.fragmentspike;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import com.gte.fragmentspike.databinding.ActivityMainBinding;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavController navController;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        assert navHostFragment != null;
+        navController = navHostFragment.getNavController();
+
+        setSupportActionBar(binding.toolbar);
+        NavigationUI.setupActionBarWithNavController(this,navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return navController.navigateUp() || super.onSupportNavigateUp();
     }
 }
